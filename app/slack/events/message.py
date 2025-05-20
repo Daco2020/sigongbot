f6c8cfe505bc4f30d702fe646716ca39062fb76d
Daco2020 <dacokim32@gmail.com>
2025-05-20 14:12:44 +0900

    fix: 스레드는 문의 알림이 뜨지 않도록 수정

--- app/slack/events/message.py
+++ app/slack/events/message.py
@@ -14,13 +14,27 @@ async def handle_message(
     """메시지 이벤트 처리"""
     await ack()
 
+    is_thread = _is_thread_message(body)
+
     # 문의 채널에 메시지 남길 시 관리자 채널에 알림 유저 이름을 알림으로 준다.
     if (
         body["event"]["channel"] == settings.SUPPORT_CHANNEL
         and body["event"]["type"] == "message"
         and not body["event"].get("subtype")
+        and not is_thread
     ):
         await client.chat_postMessage(
             channel=settings.ADMIN_CHANNEL,
             text=f"👋 <@{body['event']['user']}> 님이 <#{settings.SUPPORT_CHANNEL}> 에 문의를 남겼어요. 👀 <@{settings.ADMIN_IDS[0]}> <@{settings.ADMIN_IDS[1]}>",
         )
+
+
+def _is_thread_message(body: MessageBodyType) -> bool:
+    """
+    메시지가 스레드에 속한 메시지인지 여부를 반환합니다.
+    """
+    event = body.get("event", {})
+    thread_ts = event.get("message", {}).get("thread_ts")  # type: ignore
+    ts = event.get("message", {}).get("ts")  # type: ignore
+    is_thread = thread_ts != ts if thread_ts else False
+    return is_thread
